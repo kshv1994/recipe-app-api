369b5d2aa3ea77c1d80bc9cb5a50e3dff77936a4
keshav <kshv1994@gmail.com>
2020-11-06 23:46:13 +0530

    added list recioe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from recipe import serializers
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
@@ -33,3 +33,19 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     """manage ingredients"""
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """manage recipe viewset"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """create a new recipe object"""
+        serializer.save(user=self.request.user)
